49139d5a870390d6bda408dcbe431755311382cb
Kenston O'Neal <koneal2013@gmail.com>
2022-11-02 16:53:47 -0400

    finishing tests

--- internal/log/log_test.go
+++ internal/log/log_test.go
@@ -1,10 +1,14 @@
 package log
 
 import (
+	"io"
 	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/proto"
+
+	log_v1 "github.com/koneal2013/proglog/api/v1"
 )
 
 func TestLog(t *testing.T) {
@@ -31,21 +35,77 @@ func TestLog(t *testing.T) {
 }
 
 func testTruncate(t *testing.T, log *Log) {
+	append := &log_v1.Record{Value: []byte("hello world")}
+	for i := 0; i < 3; i++ {
+		_, err := log.Append(append)
+		require.NoError(t, err)
+	}
+	err := log.Truncate(1)
+	require.NoError(t, err)
 
+	_, err = log.Read(0)
+	require.Error(t, err)
 }
 
 func testReader(t *testing.T, log *Log) {
+	append := &log_v1.Record{Value: []byte("hello world")}
+	off, err := log.Append(append)
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), off)
+
+	reader := log.Reader()
+	b, err := io.ReadAll(reader)
+	require.NoError(t, err)
+
+	read := &log_v1.Record{}
+	err = proto.Unmarshal(b[lenWidth:], read)
+	require.NoError(t, err)
+	require.Equal(t, append.Value, read.Value)
 
 }
 
 func testInitExisting(t *testing.T, log *Log) {
+	append := &log_v1.Record{Value: []byte("hello world")}
+
+	for i := 0; i < 3; i++ {
+		_, err := log.Append(append)
+		require.NoError(t, err)
+	}
+	require.NoError(t, log.Close())
+
+	off, err := log.LowestOffset()
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), off)
+	off, err = log.HighestOffset()
+	require.NoError(t, err)
+	require.Equal(t, uint64(2), off)
+
+	n, err := NewLog(log.Dir, log.Config)
+	require.NoError(t, err)
+
+	off, err = n.LowestOffset()
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), off)
+	off, err = n.HighestOffset()
+	require.NoError(t, err)
+	require.Equal(t, uint64(2), off)
 
 }
 
 func testOutOfRangeErr(t *testing.T, log *Log) {
+	read, err := log.Read(1)
+	require.Nil(t, read)
+	require.Error(t, err)
 
 }
 
 func testAppendRead(t *testing.T, log *Log) {
+	append := &log_v1.Record{Value: []byte("hello world")}
+	off, err := log.Append(append)
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), off)
 
+	read, err := log.Read(off)
+	require.NoError(t, err)
+	require.Equal(t, append.Value, read.Value)
 }
